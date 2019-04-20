b7ac6a881d3b04d304da84f7d928cffd5b8f4716
Henry <henry@reed.edu>
2019-04-20 11:25:36 -0700

    instantiated StateT as monoid

--- MonadTransformers/parser.hs
+++ MonadTransformers/parser.hs
@@ -1,18 +1,31 @@
+{-# LANGUAGE UndecidableInstances #-}
+
 import Control.Monad.Trans.State
+import Control.Monad.Trans.Class
 
--- a parser is just an instance of a StateT
-type Parser = StateT String []
+type Parser a = StateT String [] a
 
--- running a parser is just running a generic state
--- and extracting the output
-runParser :: Parser a -> String -> [a]
-runParser p s = [ x | (x, "") <- runStateT p s ]
+run_parser :: Parser a -> String -> [(a, String)]
+run_parser = runStateT
 
--- an example parser
--- parse_head will only parse if the input is a single character
-parse_head :: Parser Char
-parse_head = do
+instance (Monad m, Monoid (m (a, s))) => Monoid (StateT s m a) where
+  mempty = StateT mempty
+  mappend st st' = StateT $ \s -> let mxs = runStateT st s ; mxs' = runStateT st' s in mappend mxs mxs'
+
+parse_char_predicate :: (Char -> Bool) -> Parser Char
+parse_char_predicate p = do
   c:cs <- get
-  put cs
-  return c
+  if p c
+    then put cs >> return c
+    else lift []
 
+parse_char :: Char -> Parser Char
+parse_char c = parse_char_predicate (== c)
+
+parse_string :: String -> Parser String
+parse_string s = case s of
+  ""   -> return ""
+  c:cs -> parse_char c >> parse_string cs
+
+-- many :: Parser a -> Parser [a]
+-- many p = many1 ++ 
